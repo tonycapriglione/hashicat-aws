490cb4fc650030f25efacb26eb2888952a8b3c2f
Anthony Capriglione <Anthony.Capriglione@ally.com>
2020-03-03 19:04:04 +0000

    Update

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource aws_instance "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Billable = "true"
-    Departmendt = "devops"
+    Department = "devops"
   }
 }
 
